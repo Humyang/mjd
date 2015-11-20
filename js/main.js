812bb4dcd98cc7ab7560e02e20f810f3ecd4c3de
杨雄贵 <378029633@qq.com>
2015-11-21 04:53:49 +0800

    init
    完成了顶部搜索栏，轮播图片，和快捷进入

---
+++ js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$('.banner').unslider({
+
+		autoplay: true,
+		arrows: false,
+		dots: true,
+		fluid: true
+	});
+	$('body').css('display', 'inherit');
+});
