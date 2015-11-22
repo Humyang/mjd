626d741cc2046a96936bb69d8dec0be2a770bafb
杨雄贵 <378029633@qq.com>
2015-11-23 02:07:53 +0800

    完成猜你喜欢

--- js/main.js
+++ js/main.js
@@ -8,25 +8,25 @@ var timerApi = {
 
 		var differTime = timerApi.seckillOverTime - nowTime;
 
-		var hours = Math.floor((differTime/1000/60/60) % 60,1);
+		var hours = Math.floor((differTime / 1000 / 60 / 60) % 60, 1);
 		var hoursUnint = hours % 10;
 		var hoursTens = (hours - hoursUnint) / 10;
 
-		var minutes = Math.floor((differTime/1000/60) % 60,0);
+		var minutes = Math.floor((differTime / 1000 / 60) % 60, 0);
 		var minutesUnint = minutes % 10;
 		var minutesTens = (minutes - minutesUnint) / 10;
 
-		var seconds = Math.floor((differTime/1000) % 60,0);
+		var seconds = Math.floor((differTime / 1000) % 60, 0);
 		var secondsUnint = seconds % 10;
 		var secondsTens = (seconds - secondsUnint) / 10;
 
 		return [
-		hoursTens,
-		hoursUnint,
-		minutesTens,
-		minutesUnint,
-		secondsTens,
-		secondsUnint
+			hoursTens,
+			hoursUnint,
+			minutesTens,
+			minutesUnint,
+			secondsTens,
+			secondsUnint
 		];
 	}
 };
@@ -42,17 +42,57 @@ jQuery(document).ready(function($) {
 
 	timerApi.seckillOverTime = new Date();
 	timerApi.seckillOverTime.setHours(23);
-timerApi.seckillOverTime.setMinutes(00);
-timerApi.seckillOverTime.setSeconds(00);
+	timerApi.seckillOverTime.setMinutes(00);
+	timerApi.seckillOverTime.setSeconds(00);
 
-var seckill_floor_title_timer_number = $(".seckill-floor-title-timer_number");
+	var seckill_floor_title_timer_number = $(".seckill-floor-title-timer_number");
 
-	setInterval(function(){
+	setInterval(function() {
 		var resArr = timerApi.seckillTimer()
-		for (i in resArr){
+		for (i in resArr) {
 			seckill_floor_title_timer_number.eq(i).text(resArr[i]);
 		}
-	},1000);
+	}, 1000);
+
+	//设置主题馆的轮播动画隐藏
+	$(".cycle-img-box").each(function() {
+		$(this).children('img').eq(1).hide();
+		$(this).children('img').eq(2).hide();
+		//10秒后隐藏 0 2，显示 1
+		//20秒售隐藏 0 1，显示 2
+		//30秒后隐藏 1 2，显示 0
+	});
+
+	// 设置主题馆的轮播动画
+	//延迟设置各个动画的开始时间，出现顺序的效果
+	 firstImageArray = $(".cycle-img-box img:nth-child(1)");
+	 secondImageArray = $(".cycle-img-box img:nth-child(2)");
+	 thirdImageArray = $(".cycle-img-box img:nth-child(3)");
+
+	loopImage();
+
+	setInterval(loopImage, 3000);
 
 	$('body').css('display', 'inherit');
 });
+
+function loopImage() {
+	secondImageArray.each(function(index, el) {
+		setTimeout(function() {
+			secondImageArray.eq(index).parent().children().hide();
+			secondImageArray.eq(index).fadeIn(500);
+		}, 1000 + index * 100);
+	});
+	thirdImageArray.each(function(index, el) {
+		setTimeout(function() {
+			thirdImageArray.eq(index).parent().children().hide();
+			thirdImageArray.eq(index).fadeIn(500);
+		}, 2000 + index * 100);
+	});
+	firstImageArray.each(function(index, el) {
+		setTimeout(function() {
+			firstImageArray.eq(index).parent().children().hide();
+			firstImageArray.eq(index).fadeIn(500);
+		}, 3000 + index * 100);
+	});
+}
